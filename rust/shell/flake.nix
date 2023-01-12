adf8d3b97f19e06baaba92d1f4693873e9a4fc1d
Will Ruggiano <wmruggiano@gmail.com>
2023-01-12 11:27:17 -0700

    feat(rust): flake-parts

--- rust/shell/flake.nix
+++ rust/shell/flake.nix
@@ -1,30 +1,47 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-nix.url = "github:cachix/pre-commit-hooks.nix";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-    self,
-    flake-utils,
-    nixpkgs,
-    rust-overlay,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          rust-overlay.overlays.default
-        ];
-      };
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.pre-commit-nix.flakeModule
+      ];
 
-      toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-    in {
-      devShells.default = pkgs.mkShell {
-        name = "rust-nightly";
-        buildInputs = [toolchain];
+      systems = ["x86_64-linux" "aarch64-darwin"];
+      perSystem = {
+        config,
+        system,
+        ...
+      }: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [inputs.rust-overlay.overlays.default];
+        };
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "rust-nightly";
+          buildInputs = [toolchain];
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+          '';
+        };
+
+        formatter = pkgs.alejandra;
+
+        pre-commit = {
+          settings = {
+            hooks.alejandra.enable = true;
+            hooks.rustfmt.enable = true;
+            hooks.clippy.enable = true;
+            hooks.cargo-check.enable = true;
+          };
+        };
       };
-    });
+    };
 }
